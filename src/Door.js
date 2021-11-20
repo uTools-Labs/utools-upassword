6253b4adc94f9fa34e9e67777e82dd6633b7f1c2
wunaiquan <wunaiquan@126.com>
2021-11-20 21:06:00 +0800

    开门密码非英文输入法禁止输入，及大小写锁定提示

--- src/Door.js
+++ src/Door.js
@@ -2,17 +2,21 @@ import React from 'react'
 import SubdirectoryArrowLeftIcon from '@mui/icons-material/SubdirectoryArrowLeft'
 import IconButton from '@mui/material/IconButton'
 import Button from '@mui/material/Button'
+import InputBase from '@mui/material/InputBase'
 import Reset from './Reset'
 
 export default class Door extends React.Component {
   state = {
     fail: false,
-    resetPassword: false
+    passwordValue: '',
+    resetPassword: false,
+    isCapsLock: false,
+    isComposition: false
   }
 
   handleEnter = () => {
     if (this.state.fail) return
-    this.props.onVerify(this.passwordInput.value, () => {
+    this.props.onVerify(this.state.passwordValue, () => {
       this.setState({ fail: true })
       setTimeout(() => {
         this.setState({ fail: false })
@@ -20,7 +24,24 @@ export default class Door extends React.Component {
     })
   }
 
+  handleInputChange = (event) => {
+    if (this.state.isComposition) return
+    this.setState({ passwordValue: event.target.value })
+  }
+
   handleInputKeydown = (event) => {
+    if (event.getModifierState('CapsLock')) {
+      if (!this.state.isCapsLock) this.setState({ isCapsLock: true })
+    } else {
+      if (this.state.isCapsLock) this.setState({ isCapsLock: false })
+    }
+    if (event.keyCode === 229) {
+      if (!this.state.isComposition) this.setState({ isComposition: true })
+      event.target.blur()
+      setTimeout(() => { event.target.focus() }, 300)
+      return
+    }
+    if (this.state.isComposition) this.setState({ isComposition: false })
     if (event.keyCode !== 13) return
     event.preventDefault()
     this.handleEnter()
@@ -35,17 +56,29 @@ export default class Door extends React.Component {
   }
 
   render () {
-    const { fail, resetPassword } = this.state
+    const { fail, resetPassword, passwordValue, isCapsLock, isComposition } = this.state
     if (resetPassword) return <Reset onOut={this.handleResetOut} />
     return (
       <div className={'door-body' + (fail ? ' door-fail' : '')}>
         <div>
           <div className={'door-input' + (fail ? ' door-swing' : '')}>
-            <input ref={c => { this.passwordInput = c }} autoFocus onKeyDown={this.handleInputKeydown} type='password' placeholder='开门密码' />
-            <div>
+            <InputBase
+              autoFocus
+              fullWidth
+              type='password'
+              placeholder='开门密码'
+              value={passwordValue}
+              onKeyDown={this.handleInputKeydown}
+              onChange={this.handleInputChange}
+            />
+            <div className='door-input-enter'>
               <IconButton onClick={this.handleEnter}>
                 <SubdirectoryArrowLeftIcon />
               </IconButton>
+            </div>
+            <div className='door-tooltip'>
+              {isCapsLock && <div>键盘大写锁定已打开</div>}
+              {isComposition && <div>请切换到英文输入法</div>}
             </div>
           </div>
         </div>
